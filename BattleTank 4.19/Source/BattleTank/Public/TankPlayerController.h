a52123206e46c1a81de208ce1bd3634068bbe756
Ali El Saleh <elsaleh78@gmail.com>
2018-05-23 16:51:18 +1000

    Made GetControlledTank a UFUNCTION for blueprint, so that I can change the color of the crosshair programatically. Nothing too drastic of change.

--- BattleTank 4.19/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank 4.19/Source/BattleTank/Public/TankPlayerController.h
@@ -14,11 +14,13 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
-	ATank* GetControlledTank() const;
-	
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void BeginPlay() override;
+
+protected:
+	UFUNCTION(BlueprintCallable, Category = Setup)
+		ATank* GetControlledTank() const;
 
 private:
 	// Start the tank moving the barrel so that a shot would hit where
@@ -38,5 +40,4 @@ private:
 
 	UPROPERTY(EditDefaultsOnly)
 		float LineTraceRange = 1000000.0;
-
 };
