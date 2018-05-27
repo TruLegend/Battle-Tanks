95c123d3bb8b07dc5cba79298941f0034bb53e41
Ali El Saleh <elsaleh78@gmail.com>
2018-05-27 16:38:46 +1000

    Readded what I deleted in the last commit by accident.

--- BattleTank 4.19/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank 4.19/Source/BattleTank/Public/TankPlayerController.h
@@ -16,6 +16,10 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
+protected:
+	UFUNCTION(BlueprintImplementableEvent, Category = "Setup")
+		void FoundAimingComponent(UTankAimingComponent* AimComRef);
+
 private:
 	// Start the tank moving the barrel so that a shot would hit where
 	// the crosshair intersects the world.
