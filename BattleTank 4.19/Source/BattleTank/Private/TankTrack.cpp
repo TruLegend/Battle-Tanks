240035c9939bd315308cf16ed0fa29dc9fb13b64
Ali El Saleh <elsaleh78@gmail.com>
2018-05-28 14:45:15 +1000

    Reduce the slipperyness of the tank moving sideways, BUT the tank now trembles for some reason.

--- BattleTank 4.19/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank 4.19/Source/BattleTank/Private/TankTrack.cpp
@@ -3,6 +3,23 @@
 #include "BattleTank.h"
 #include "TankTrack.h"
 
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankTrack::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	
+	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2; // Two Tracks
+
+	TankRoot->AddForce(CorrectionForce);
+}
 
 void UTankTrack::SetThrottle(float Throttle)
 {
